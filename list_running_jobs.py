756864b65e7dde4d9b951a74eb076092e948e1dc
Philliams <phillip.mitchel.williams@gmail.com>
2025-06-06 19:53:41 -0400

    running against env

---
+++ list_running_jobs.py
@@ -0,0 +1,10 @@
+import sys
+import json
+
+# main
+if __name__ == "__main__":
+    data = sys.stdin.read()
+    data = json.loads(data)
+    print("Workflow Runs:")
+    for workflow in data['workflow_runs']:
+        print(f"\t --  {workflow['name']} | {workflow['id']} | {workflow['status']}")
---
+++ makefile
@@ -0,0 +1,2 @@
+gha-runs:
+	@gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" repos/Philliams/test-gha-envs/actions/runs
